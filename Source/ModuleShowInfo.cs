3a485ff0ac42f42d99ea25c580ce7b10dbb1416c
Johny Sith <thefuryx@gmail.com>
2017-09-20 22:04:46 +0200

    .

---
+++ Source/ModuleShowInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace RP0
+{
+    public class ModuleShowInfo : PartModule
+    {
+        public override string GetInfo()
+        {
+            string tmp = "";
+            try
+            {
+                tmp += "Part name: " + part.name;
+                // throws - tmp += "\nTech Required: " + part.partInfo.TechRequired;
+                // throws - tmp += "\nEntry Cost: " + part.partInfo.entryCost;
+            }
+            catch (Exception e)
+            {
+                Debug.Log("**RP0 error getting info, exception " + e);
+            }
+            return tmp;
+        }
+    }
+}
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RP0")]
+[assembly: AssemblyDescription("Plugin for RP-0 mod")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KSP-RO")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright ©  2014 CC-BY-NC-SA 4.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36d2b381-e48b-4938-8919-15973b32ce5a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.55.0.0")]
+[assembly: AssemblyFileVersion("0.55.0.0")]
+
+[assembly: KSPAssembly("RP-0", 0, 55)]
+[assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
